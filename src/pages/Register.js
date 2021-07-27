b8225f45c3c8e9a796973b909f0f37a368cdf442
Youngpsc <olarescu.vlad@gmail.com>
2021-07-27 19:19:56 +0300

    session hooks applied

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSession } from "../hooks";
+import { useHistory } from "react-router-dom";
 
 export const Register = () => {
+  const { loading, loggedIn } = useSession();
+  const history = useHistory();
+
+  useEffect(() => {
+    !loading && loggedIn && history.push("/");
+  }, [loading, loggedIn, history]);
+
   return (
     <div>
       <h1>Register</h1>
