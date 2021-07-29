bf18fe6677d8ac4dcbb5352c5edba79a5a5af09c
Youngpsc <olarescu.vlad@gmail.com>
2021-07-29 20:29:20 +0300

    add login page and components

---
+++ src/consts/const.js
@@ -0,0 +1,20 @@
+export const colors = {
+  white: "#edf2f4",
+  text: {
+    primary: "#2b2d42",
+    secondary: "#8d99ae",
+  },
+  red: {
+    primary: "#d90429",
+    secondary: "#ef233c",
+  },
+};
+
+export const BUTTON_TYPES = {
+  PRIMARY: "primary",
+  SECONDARY: "secondary",
+};
+
+export const emailRegex =
+  // eslint-disable-next-line
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSession } from "../hooks";
 import { useHistory } from "react-router-dom";
+import { Input, Button, Form, Typography } from "../components";
+import { BUTTON_TYPES, emailRegex } from "../consts/const";
+import { api } from "../api/api";
 
 export const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputValueChange = (key, value) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [key]: value,
+    }));
+  };
+
+  const handleFormError = (key, value) => {
+    setErrors((prevState) => ({
+      ...prevState,
+      [key]: value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    console.log(formData);
+
+    if (!emailRegex.test(formData.email)) {
+      handleFormError("email", "Invalid email address.");
+      return;
+    }
+
+    if (formData.password.length < 8) {
+      handleFormError("password", "Password must be at least 8 characters.");
+      return;
+    }
+
+    const { data, status } = await api.login(formData.email, formData.password);
+
+    if (status === 201) {
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("account", JSON.stringify(data.user));
+      setTimeout(() => {
+        history.push("/");
+      }, 100);
+    } else {
+      console.warn("ERROR: ", data);
+    }
+  };
+
   const { loading, loggedIn } = useSession();
   const history = useHistory();
 
@@ -11,8 +63,29 @@ export const Login = () => {
   }, [loading, loggedIn, history]);
 
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Form>
+      <Typography.H1>Login</Typography.H1>
+      <Input
+        placeholder="Email address"
+        type="email"
+        error={errors.email}
+        value={formData.email}
+        handleOnChange={handleInputValueChange}
+        name="email"
+      />
+      <Input
+        placeholder="Password"
+        type="password"
+        error={errors.password}
+        value={formData.password}
+        handleOnChange={handleInputValueChange}
+        name="password"
+      />
+      <Button
+        buttonType={BUTTON_TYPES.PRIMARY}
+        text="Log in"
+        handleClick={handleSubmit}
+      />
+    </Form>
   );
 };
